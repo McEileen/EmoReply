204fd9b414f05b33eaceb6426729aa4f43ea309c
jbrew <brew.jamie@gmail.com>
2019-02-22 14:41:14 -0500

    split text into sentences

--- librarian.py
+++ librarian.py
@@ -1,4 +1,33 @@
 with open('Perks.txt') as infile:
     text = infile.read()
 
-print(text)
+
+
+def get_sentences(text):
+	paragraphs = text.split('\n')
+	print(len(paragraphs))
+	all_sentences = []
+
+	for p in paragraphs:
+		sentences = p.split('. ')
+		all_sentences += sentences
+
+	return all_sentences
+
+
+# returns the sentences from a list of sentencess that SEEM like good sentences
+#	current filters: must be uppercase
+def filter_sentences(sents):
+	return [s for s in sents if len(s) > 0 and s[0] == s[0].upper()]
+
+
+
+
+sents = get_sentences(text)
+filtered = filter_sentences(sents)
+
+
+print(len(sents))
+print(len(filtered))
+
+print(filtered[200:300])
