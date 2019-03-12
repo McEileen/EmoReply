6d7d66648ee66992c48a84d139c3ac6de687d8ad
jbrew <brew.jamie@gmail.com>
2019-03-12 17:32:11 -0400

    display default text

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,3 +1,11 @@
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
 chrome.runtime.onInstalled.addListener(function() {
-    // add an action here
+    var text = document.getElementById('inputField').value;
+    console.log('Text', text);
+    //var text = $("#myTextArea").val();
 });
+
+function buttonClicked() {
+	chrome.extension.getBackgroundPage().console.log("button clicked!");
+}
