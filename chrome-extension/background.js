003e81f27bb259b877b7c60e6038080c7b3649a1
McEileen <eileenm@gmail.com>
2019-03-12 18:18:38 -0400

    manipulate user input

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,11 +1,8 @@
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-chrome.runtime.onInstalled.addListener(function() {
-    var text = document.getElementById('inputField').value;
-    console.log('Text', text);
-    //var text = $("#myTextArea").val();
-});
-
-function buttonClicked() {
-	chrome.extension.getBackgroundPage().console.log("button clicked!");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('what is document', document);
+    var button = document.getElementById('submit');
+    
+    button.addEventListener('click', function(event) {
+        console.log("CLICKED THE BUTTON!")
+    });
+})
