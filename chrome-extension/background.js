b9f140021714fb956a5fc8eb8babcc101f91a32f
McEileen <eileenm@gmail.com>
2019-03-15 17:01:03 -0400

    WIP - console log text from inputField

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('what is document', document);
-    var button = document.getElementById('submit');
-    
+    const button = document.getElementById('submit');
+    console.log('see the button', button);
+    const inputField = document.getElementById('inputField');
+
     button.addEventListener('click', function(event) {
+        console.log('see button inside event', button)
         console.log("CLICKED THE BUTTON!")
+        console.log(inputField.value);
     });
 })
